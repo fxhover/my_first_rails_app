598e69a99b479736355b5008f97ca79a8636d09f
fxhover <736698959@qq.com>
2014-08-22 08:22:15 +0800

    delete home dir

--- home/fangxiang/www/blog/blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'f60e7f827b0f6c49ab6e6538219615852a8ee322028cedf68c1b3da160aaea8b24d3ce39abc66920ad9871ab9276f47640d502a7a22b367cf477a9cdaf0aef4e'
